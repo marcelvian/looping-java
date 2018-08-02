41028b92589a256c703b9af8075c53afe9df6ad7
ASUS <ASUS@DESKTOP-K3F0R94>
2018-08-02 07:59:38 +0700

    For while Do-While

--- src/jobsheet8/Contoh1.java
+++ src/jobsheet8/Contoh1.java
@@ -10,9 +10,31 @@ public class Contoh1 {
     {
         //PERULANGAN FOR
         int x;
-        for(x = 1; x )
+        for(x = 1; x <= 5; x++);
+        {
+            System.out.println("SMK Telkom Malang");
+        }
+        
+        //PERULANGAN WHILE
+        int i = 1;
+        while(i <= 5)
+        {
+            System.out.println("SMK Telkom Malang");
+            i++;
+        }
+        
+        //PERULANGAN DO WHILE
+        int j = 1;
+        do
+        {
+            System.out.println("SMK Telkom Malang");
+            j++;
+        }
+        while(j <= 5);
+        }
     }
-       }
+    
+       
      
     
     
