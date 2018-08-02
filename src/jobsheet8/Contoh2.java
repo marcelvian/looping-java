19f16a50d908663146fc8a44dc994e8a01293448
ASUS <ASUS@DESKTOP-K3F0R94>
2018-08-02 08:23:41 +0700

    For while Do-While

---
+++ src/jobsheet8/Contoh2.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet8;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Contoh2 {
+    {
+        int x;
+        for(x = 1; x <= 10; x++)
+        {
+            System.out.print(x + " ");
+            x++;
+        }
+        
+        x = 1;
+        while(x <= 10)
+        {
+            System.out.print(x + " ");   
+        }
+        
+        System.out.println();
+        
+        x = 1;
+        do
+        {
+         System.out.println(x + " ");
+        }
+        while(x <= 10);
+    }
+}
