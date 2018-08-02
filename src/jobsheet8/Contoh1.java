66e7afb47b36e0289e2333dd54596d1431121387
ASUS <ASUS@DESKTOP-K3F0R94>
2018-08-02 07:47:18 +0700

    For while Do-While

---
+++ src/jobsheet8/Contoh1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet8;
+
+public class Contoh1 {
+    public static void main(String[] args) 
+    {
+        //PERULANGAN FOR
+        int x;
+        for(x = 1; x )
+    }
+       }
+     
+    
+    
+    
+
